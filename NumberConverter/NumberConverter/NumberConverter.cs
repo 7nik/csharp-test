e658d5c5ea527154c28d1e2f95b7ff6e0a2a9b12
7nik <kfiirnet@gmail.com>
2020-02-07 17:31:47 +0200

    added task 1

---
+++ NumberConverter/NumberConverter/NumberConverter.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConverter
+{
+    // class for converting string to number and back for any valid radix (base)
+    class NumberConverter
+    {
+
+        private enum State
+        {
+            Ok,
+            UnsupportedRadix,
+            ParseException,
+            OverflowException
+        }
+
+        // alphabet of digits
+        public readonly string Alphabet;
+
+        public NumberConverter(string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ") => Alphabet = alphabet;
+
+        // check wether the radix is valid for current alphabet
+        public bool IsValidRadix(int radix) => radix > 1 && radix <= Alphabet.Length;
+
+        // main parsing function
+        // return 
+        private (State state, long value) _parse(string str, int radix, bool caseSensitive)
+        {
+            if (!IsValidRadix(radix)) return (State.UnsupportedRadix, 0);
+            string alphabet = Alphabet;
+            // for case insensitive parsing make both strings in upper case
+            if (!caseSensitive)
+            {
+                alphabet = Alphabet.ToUpper();
+                str = str.ToUpper();
+            }
+            long value = 0;
+            long power = 1;
+            try
+            {
+                for (int i = str.Length-1; i >= 0; i--)
+                    checked
+                    {
+                        int digit = alphabet.IndexOf(str[i]);
+                        if (digit < 0 || digit >= radix) return (State.ParseException, value);
+                        value += power * digit;
+                        power *= radix;
+                    }
+            } catch
+            {
+                return (State.OverflowException, value);
+            }
+            return (State.Ok, value);
+        }
+
+        // parse string and return wether it was successful
+        public bool TryParse(string str, int radix, out long value, bool caseSensitive = false)
+        {
+            State state;
+            (state, value) = _parse(str, radix, caseSensitive);
+            return state == State.Ok;
+        }
+
+        // either returns parsed number or throws corresponding exception
+        public long Parse(string str, int radix, bool caseSensitive = false)
+        {
+            var (state, value) = _parse(str, radix, caseSensitive);
+            switch (state)
+            {
+                case State.Ok:
+                    return value;
+                case State.OverflowException:
+                    throw new OverflowException("The number is over 64 bits in size.");
+                case State.ParseException:
+                    throw new FormatException("The number contains invalid digit.");
+                case State.UnsupportedRadix:
+                    throw new ArgumentOutOfRangeException("The radix is less 2 or above the alpahabet.");
+                default:
+                    throw new Exception("Unknown state.");
+            }
+        }
+
+        // converts given number to string accorting to the radix
+        public string Stringify(long value, int radix)
+        {
+            if (radix < 2 || radix >= Alphabet.Length) throw new ArgumentOutOfRangeException("The base is less 2 or above the alpahabet.");
+            if (value < 0) throw new ArgumentOutOfRangeException("Negative numbers aren't supported.");
+            if (value == 0) return Alphabet.Substring(0, 1);
+            StringBuilder str = new StringBuilder((int)(Math.Log(value, radix)) + 1);
+            while (value > 0)
+            {
+                str.Insert(0, Alphabet[(int)(value % radix)]);
+                value = (int)(value / radix);
+            }
+            return str.ToString();
+        }
+    }
+
+    class Task1 
+    { 
+        static void Main(string[] args)
+        {
+            NumberConverter converter = new NumberConverter();
+            // get source radix
+            Console.Write("Enter source number base: ");
+            int sourceRadix;
+            if (!ReadRadix(converter.IsValidRadix, out sourceRadix)) return;
+            // get target radix
+            Console.Write("Enter target number base: ");
+            int targetRadix;
+            if (!ReadRadix(converter.IsValidRadix, out targetRadix)) return;
+
+            if (sourceRadix == targetRadix)
+            {
+                Console.WriteLine("Useless convertation.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Write("Enter the number: ");
+            string sourceNumber = Console.ReadLine();
+            // try to parse the number and stringify to target radix
+            if (converter.TryParse(sourceNumber, sourceRadix, out long number))
+            {
+                string targetNumber = converter.Stringify(number, targetRadix);
+                Console.WriteLine($"Your result is {targetNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Couldn't parse or stringify the number.");
+            }
+
+            Console.ReadLine();
+        }
+
+        // reads radix and check wether it's valid
+        static bool ReadRadix(Func<int, bool> check, out int radix)
+        {
+            string strBase = Console.ReadLine();
+            switch (strBase)
+            {
+                case "b":
+                case "binary":
+                    radix = 2;
+                    break;
+                case "o":
+                case "octal":
+                    radix = 8;
+                    break;
+                case "d":
+                case "decimal":
+                    radix = 10;
+                    break;
+                case "h":
+                case "hex":
+                case "heximal":
+                    radix = 16;
+                    break;
+                default:
+                    if (int.TryParse(strBase, out radix) && check(radix))
+                    {
+                        return true;
+                    }
+                    Console.WriteLine("This radix is not supported.");
+                    Console.ReadLine();
+                    return false;
+            }
+            return check(radix);
+
+        }
+    }
+}
---
+++ Split'n'Swap Array/Split'n'Swap Array/Split'n'Swap Array.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_n_Swap_Array
+{
+    class Split_n_SwapArray
+    {
+        static void Main(string[] args)
+        {
+            Split_n_SwapLinearArray();
+            Split_n_SwapMatrixArray();
+
+            Console.ReadLine();
+        }
+
+        // reads and validates array size
+        static bool ReadSize(out int size)
+        {
+            if (int.TryParse(Console.ReadLine(), out size) && size > 1)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid size.");
+                return false;
+            }
+        }
+
+        // swap two parts of an array
+        static void SwapArray<T>(T[] arr, int position)
+        {
+            T[] arr2 = new T[arr.Length];
+            arr.CopyTo(arr2, 0);
+            Array.Copy(arr2, position, arr, 0, arr.Length - position);
+            Array.Copy(arr2, 0, arr, arr.Length - position, position);
+        }
+
+        static void Split_n_SwapLinearArray()
+        {
+            // read array size and split position
+            Console.Write("Enter array length: ");
+            if (!ReadSize(out int length)) return;
+            Console.Write("Enter split position: ");
+            int.TryParse(Console.ReadLine(), out int position);
+            if (position < 1 || position >= length)
+            {
+                Console.WriteLine("Invalid position.");
+                Console.ReadLine();
+                return;
+            }
+
+            // generate and print the array
+            Console.WriteLine("Array was:");
+            int[] arr = new int[length];
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = random.Next(100);
+                Console.Write($"{arr[i]}\t");
+            }
+            Console.WriteLine();
+
+            // split and swap array
+            SwapArray(arr, position);
+            // print array
+            Console.WriteLine("Array became:");
+            Console.WriteLine(string.Join("\t", arr));
+            Console.WriteLine();
+        }
+
+        static void Split_n_SwapMatrixArray()
+        {
+            // read matrix sizes and split position
+            Console.Write("Enter matrix width: ");
+            if (!ReadSize(out int width)) return;
+            Console.Write("Enter matrix height: ");
+            if (!ReadSize(out int height)) return;
+            Console.Write("Split vertically? (y/n): ");
+            bool vertically = "y" == Console.ReadLine();
+
+            Console.Write("Enter split position: ");
+            int.TryParse(Console.ReadLine(), out int position);
+            if (position < 1 || vertically && position >= width || !vertically && position >= height)
+            {
+                Console.WriteLine("Invalid position.");
+                Console.ReadLine();
+                return;
+            }
+
+            // generate and print matrix
+            Console.WriteLine("Array was:");
+            int[][] arr = new int[width][];
+            Random random = new Random();
+            for (int i = 0; i < width; i++)
+            {
+                arr[i] = new int[height];
+                for (int j = 0; j < height; j++)
+                {
+                    arr[i][j] = random.Next(100);
+                    Console.Write($"{arr[i][j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // split and swap matrix
+            if (vertically)
+            {
+                foreach (int[] row in arr) SwapArray(row, position);
+            }
+            else
+            {
+                SwapArray(arr, position);
+            }
+
+            // print matrix
+            Console.WriteLine("Array became:");
+            foreach (int[] row in arr) Console.WriteLine(string.Join("\t", row));
+            Console.WriteLine();
+        }
+
+    }
+}
